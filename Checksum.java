cc7be5ad2f39652c5a8593a4f708bf3b3b4a89af
Riken7 <rikenprajapati2005@gmail.com>
2024-05-20 13:57:40 +0530

    ..

--- Checksum.java
+++ Checksum.java
@@ -1,72 +1,58 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Checksum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the data string : ");
+        System.out.print("Enter data to send: ");
         String data = sc.nextLine();
-        int[] ascii = Ascii(data);
-        String[] binary = Binary(ascii);
-        for(int i=0 ; i<ascii.length ; i++) {
-            System.out.print(ascii[i] + " => Binary  : " + binary[i] +"\n" );
+
+        ArrayList<Integer> ascii = ascii(data);
+        ArrayList<String> binary = binary(ascii);
+
+        System.out.println(ascii);
+        System.out.println(binary);
+
+        String checksum = checksum(binary);
+        binary.add(0, checksum);
+
+        System.out.println(checksum);
+
+        System.out.println("Receiver side");
+        String result = checksum(binary);
+        System.out.println(result);
+
+        if(result.equals("00000000")) {
+            System.out.println("Data is correct");
+        } else {
+            System.out.println("Data is incorrect");
         }
-        // String[] binary = {"10101001","00111001"};
-        BitAdd(binary); 
     }
-    public static int[] Ascii(String data){
-        int[] ascii = new int[data.length()];
-        for(int i=0; i<data.length(); i++){
-            ascii[i] = (int)data.charAt(i);
+
+    public static ArrayList<Integer> ascii(String data) {
+        ArrayList<Integer> ascii = new ArrayList<>();
+        for (int i = 0; i < data.length(); i++) {
+            ascii.add((int) data.charAt(i));
         }
         return ascii;
     }
-    public static String[] Binary(int[] ascii){
-        String[] binary = new String[ascii.length];
-        for(int i=0; i<ascii.length; i++){
-            binary[i] = String.format("%8s", Integer.toBinaryString(ascii[i])).replace(' ', '0');
-    
+
+    public static ArrayList<String> binary(ArrayList<Integer> ascii) {
+        ArrayList<String> binary = new ArrayList<>();
+        for (int i = 0; i < ascii.size(); i++) {
+            binary.add(String.format("%8s", Integer.toBinaryString(ascii.get(i))).replace(" ", "0"));
         }
         return binary;
     }
-    public static void BitAdd(String[] binary){
-        String sum = "";
-        String add = "00000000";
-        char carry = '0';
-        for(int i=0 ; i<binary.length ; i++){
-            for(int j=7 ; j>=0 ; j--){
-                if(binary[i].charAt(j) == '1' && add.charAt(j)=='1'){
-                    if(carry == '0'){
-                        carry = '1';
-                    }else if(carry == '1'){
-                        sum = "1"+sum;
-                        continue;
-                    }
-                    sum = "0"+sum ;
-                }else if(binary[i].charAt(j) == '0' && add.charAt(j)=='0'){
-                    if(carry == '1'){
-                        carry = '0';
-                        sum = "1" + sum;
-                        continue;
-                    }
-                    sum = "0"+sum;
-                }else{
-                    if(carry == '1'){
-                        sum = "0"+ sum;
-                        continue;
-                    }
-                    sum = "1"+sum;
-                }
-            }
-            add = sum;
-            sum = "";
+
+    public static String checksum(ArrayList<String> binary) {
+        int sum = 0;
+        for (String s : binary) {
+            sum += Integer.parseInt(s, 2);
         }
-        // System.out.println(add);
-        System.out.print("\nChecksum bit : ");
-        for(int i=0 ; i<add.length() ; i++){
-            if(add.charAt(i) == '0'){
-                System.out.print("1");
-            }else{
-                System.out.print("0");
-            }
+        while (sum > 0b11111111) {
+            sum = (sum & 0b11111111) + 1;
         }
+        return String.format("%8s", Integer.toBinaryString(sum ^ 0b11111111)).replace(" ", "0");
     }
 }
