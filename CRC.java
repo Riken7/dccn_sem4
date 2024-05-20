819f56faad508a372424312bd1b7e487be5b2c1d
Riken7 <rikenprajapati2005@gmail.com>
2024-05-20 11:14:07 +0530

    ..

--- CRC.java
+++ CRC.java
@@ -5,19 +5,18 @@ public class CRC {
         System.out.println("Enter the data bits: ");
         Scanner sc = new Scanner(System.in);
         String data = sc.nextLine();
-        // String data = "10110011";
+        String data2 = data;
         System.out.println("Enter the divisor: ");
         String divisor = sc.nextLine();
-        // String divisor = "10011";
         sc.close();
         for(int i=0 ; i<divisor.length()-1 ; i++) {
             data = data + "0";
         }
-        // System.out.println(data);
-        // System.out.println(divisor);
-        
         String remainder = xor(data, divisor);
         System.out.println("CRC output : " +remainder);
+        String transmittedData = data2 + remainder;
+        System.out.println(transmittedData);
+        receiver(transmittedData, divisor);
     }
     public static String xor(String data, String divisor) {
         while(data.length() >= divisor.length()) {
@@ -26,10 +25,21 @@ public class CRC {
                 temp += (data.charAt(i) ^ divisor.charAt(i));
             }
             data = temp + data.substring(divisor.length(), data.length());
-            while(data.charAt(0) == '0' ) {
+            if(data.charAt(0) == '0' ) {
                 data = data.substring(1);
             }
         }
         return data;
     }
+    public static void receiver(String transmittedData, String divisor) {
+
+        String remainder = xor(transmittedData, divisor);
+        System.out.println(remainder);
+        if (Integer.parseInt(remainder) == 0) {
+            System.out.println("Data is received without any error.");
+        } else {
+            System.out.println("Data is received with errors.");
+        }
+    }
+
 }
